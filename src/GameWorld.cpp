42eea59d3a32f02212147011c47241dd7fa314ba
Ravbug <ravbuganimations@gmail.com>
2020-08-30 20:52:04 -0700

    Update RavEngine + spinning cube

--- src/GameWorld.cpp
+++ src/GameWorld.cpp
@@ -2,22 +2,24 @@
 #include <RavEngine/CameraComponent.hpp>
 #include <RavEngine/StaticMesh.hpp>
 
+Ref<Entity> cube = new Entity();
+
 GameWorld::GameWorld()
 {
 	Ref<Entity> cameraActor = new Entity();
 	cameraActor->AddComponent<CameraComponent>(new CameraComponent())->setActive(true);
-	cameraActor->transform()->LocalTranslateDelta(vector3(0,0,0));
+	cameraActor->transform()->LocalTranslateDelta(vector3(0,0,5));
+	//cameraActor->transform()->LocalRotateDelta(vector3(0,glm::radians(30.0),0));
 
 	Spawn(cameraActor);
 
-	Ref<Entity> cube = new Entity();
+	
 	auto staticMesh = cube->AddComponent<StaticMesh>(new StaticMesh());
 	staticMesh->SetMaterial(new Material());
-	cube->transform()->LocalTranslateDelta(vector3(0,0,-5));
 	Spawn(cube);
 }
 
-void GameWorld::posttick(float)
+void GameWorld::posttick(float f)
 {
-	
+	cube->transform()->LocalRotateDelta(vector3(0, glm::radians(2.0 * f), 0));
 }
