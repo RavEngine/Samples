107da40c2c9f85b59d9e5cc4b0252b7f44faf741
Ravbug <ravbuganimations@gmail.com>
2020-09-30 22:19:09 -0700

    Main thread dispatcher RavEngine_App + dispatcher and documentation + dispatched tasks executor
    Material
    ~ trade mutex for spin lock
    
    Puck
    + empty basic script

--- AirHockey/Puck.hpp
+++ AirHockey/Puck.hpp
@@ -3,6 +3,14 @@
 #include <RavEngine/Entity.hpp>
 #include <RavEngine/BuiltinMaterials.hpp>
 #include <RavEngine/StaticMesh.hpp>
+#include <RavEngine/ScriptComponent.hpp>
+
+class BasicScript : public RavEngine::ScriptComponent{
+public:
+	void Tick(float scale) override{
+		//puck logic goes here
+	}
+};
 
 class Puck : public RavEngine::Entity{
 public:
@@ -14,6 +22,7 @@ public:
 			material = new RavEngine::DefaultMaterialInstance(RavEngine::Material::Manager::AccessMaterialOfType<RavEngine::DefaultMaterial>());
         }
         puckmesh->SetMaterial(material);
+		AddComponent<BasicScript>(new BasicScript());
         transform()->SetLocalScale(vector3(0.05,0.05,0.05));
     }
 };
