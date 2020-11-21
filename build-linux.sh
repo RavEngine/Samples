ccf38910c8d7183e8d7793f3d654be4e8f700c2d
Ravbug <ravbuganimations@gmail.com>
2020-11-21 11:18:06 -0800

    Shebangs in linux build scripts

--- build-linux.sh
+++ build-linux.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 ./init-linux.sh
 cd build;
 cmake --build . --config release --target RavEngine_Sample_AirHockey RavEngine_Sample_Playground RavEngine_Samples_TestBasics
--- init-linux.sh
+++ init-linux.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 mkdir -p build && cd build && cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release ..
