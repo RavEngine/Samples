f57777167d0a0e29c0586d319f9a91d0eb60cf37
Ravbug <ravbuganimations@gmail.com>
2020-12-18 00:06:14 -0800

    Linux compile script uses clang++

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export CC=/usr/bin/clang;
+export CXX=/usr/bin/clang++;
 ./init-linux.sh
 cd build;
 cmake --build . --config release --target RavEngine_Sample_AirHockey RavEngine_Sample_Playground RavEngine_Samples_TestBasics
