3476879e4afd983ac0eb706fc8fa5f3956bc0947
Ravbug <ravbuganimations@gmail.com>
2021-01-20 10:20:45 -0800

    Set tick rate to unlocked in Performance_C

--- Performance_C/App.cpp
+++ Performance_C/App.cpp
@@ -10,6 +10,9 @@ void Performance_C::OnStartup(int argc, char **argv){
 	RenderEngine::VideoSettings.height = 720;
 	
 	Renderer->SyncVideoSettings();
+    
+    //unlock the tickrate
+    SetMinTickTime(std::chrono::duration<double, std::milli>(0));
 	
 	SetWorld(std::make_shared<PerfC_World>());
 }
