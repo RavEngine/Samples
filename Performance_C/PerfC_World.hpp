c10f18a04728f93c4d2e6f873005f27dff173246
Ravbug <ravbuganimations@gmail.com>
2021-02-08 21:38:30 -0800

    Increase object count

--- Performance_C/PerfC_World.hpp
+++ Performance_C/PerfC_World.hpp
@@ -22,7 +22,7 @@ public:
 #ifdef _DEBUG
 	static constexpr size_t num_objects = 5000;	//reduced for demo because debug builds are slower
 #else
-	static constexpr size_t num_objects = 15000;
+	static constexpr size_t num_objects = 21000;
 #endif
 	
 	static bool TexturesEnabled;
