26224544192ea287f2f84cc4922b8be06e54c627
Ravbug <ravbuganimations@gmail.com>
2021-02-11 23:55:52 -0800

    Update build scripts

--- build-linux.sh
+++ build-linux.sh
@@ -4,4 +4,4 @@ export CC=/usr/bin/clang;
 export CXX=/usr/bin/clang++;
 ./init-linux.sh
 cd build;
-cmake --build . --config release --target RavEngine_Sample_AirHockey RavEngine_Sample_Playground RavEngine_Sample_Performance_C RavEngine_Samples_TestBasics
+cmake --build . --config release --target RavEngine_Sample_AirHockey RavEngine_Sample_GUIKitchenSink RavEngine_Sample_Playground RavEngine_Sample_Performance_C RavEngine_Samples_TestBasics
