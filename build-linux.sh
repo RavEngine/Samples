2f511228c25abbbd4f4a45189320255713909fca
Ravbug <ravbuganimations@gmail.com>
2021-02-14 13:02:50 -0800

    Use g++ on Linux instead of clang++

--- build-linux.sh
+++ build-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export CC=/usr/bin/clang;
-export CXX=/usr/bin/clang++;
+#export CC=/usr/bin/clang;
+#export CXX=/usr/bin/clang++;
 ./init-linux.sh
 cd build;
 cmake --build . --config release --target RavEngine_Sample_AirHockey RavEngine_Sample_GUIKitchenSink RavEngine_Sample_Playground RavEngine_Sample_Performance_C RavEngine_Samples_TestBasics
