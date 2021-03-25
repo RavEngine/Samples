b1825a93d337e3592a734d1591cd8c2ea85202d6
Ravbug <ravbuganimations@gmail.com>
2021-03-24 17:20:55 -0700

    Update init-ios.sh

--- init-ios.sh
+++ init-ios.sh
@@ -1,1 +1,1 @@
-mkdir -p build && cd build && cmake -Wno-dev -G "Xcode" -DCMAKE_TOOLCHAIN_FILE=../RavEngine/deps/ios/ios.toolchain.cmake -DENABLE_ARC=OFF -DDEPLOYMENT_TARGET=14.0 -DPLATFORM=TVOSCOMBINED ..
+mkdir -p build && cd build && cmake -Wno-dev -G "Xcode" -DCMAKE_TOOLCHAIN_FILE=../RavEngine/deps/ios/ios.toolchain.cmake -DENABLE_ARC=OFF -DDEPLOYMENT_TARGET=14.0 -DPLATFORM=OS64COMBINED ..
