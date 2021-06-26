b4707baadc65d4dbe3757c4f7528eb6423c06775
Ravbug <ravbuganimations@gmail.com>
2021-06-25 19:04:49 -0700

    Slider to change number of stanford dragons

--- Samples/Perf-Lighting/Level.cpp
+++ Samples/Perf-Lighting/Level.cpp
@@ -2,6 +2,8 @@
 #include <RavEngine/CameraComponent.hpp>
 #include <RavEngine/StaticMesh.hpp>
 #include <RavEngine/QueryIterator.hpp>
+#include <RavEngine/GUI.hpp>
+#include <RavEngine/InputManager.hpp>
 #include "LightEntity.hpp"
 
 using namespace RavEngine;
@@ -23,13 +25,28 @@ struct SpinSystem : public RavEngine::AutoCTTI {
 	}
 };
 
+struct ObjectMarker : public RavEngine::Component, public Queryable<ObjectMarker> {};
+
 struct StaticMeshEntity : public Entity {
 	StaticMeshEntity(Ref<MeshAsset> mesh, Ref<PBRMaterialInstance> mat) {
-		EmplaceComponent<StaticMesh>(mesh, mat);
+		EmplaceComponent<StaticMesh>(mesh, mat)->Enabled = false;
 		EmplaceComponent<SpinComponent>();
+		EmplaceComponent<ObjectMarker>();
 		constexpr float dist = 1.0;
 		transform()->LocalTranslateDelta(vector3(Random::get(-dist,dist), Random::get(-dist, dist), Random::get(-dist, dist)));
 	}
+};
+
+struct FPSSystem : public RavEngine::AutoCTTI {
+	inline constexpr auto QueryTypes() const {
+		return QueryIteratorAND<GUIComponent>();
+	}
+
+	inline void Tick(float, RavEngine::AccessReadWrite<GUIComponent> g) {
+		auto gui = g.get();
+		auto doc = gui->GetDocument("ui.rml");
+		doc->GetElementById("metrics")->SetInnerRML(StrFormat("FPS: {} ({} ms)", std::round(App::Renderer->GetCurrentFPS()),std::round(App::Renderer->GetLastFrameTime())));
+	};
 };
 
 void Level::OnActivate() {
@@ -37,7 +54,48 @@ void Level::OnActivate() {
 	// load camera and lights
 	auto camera = make_shared<Entity>();
 	camera->EmplaceComponent<CameraComponent>()->setActive(true);
-	camera->transform()->LocalTranslateDelta(vector3(0,0.5,2));
+	camera->transform()->LocalTranslateDelta(vector3(0, 0.5, 2));
+
+	// GUI
+	auto g = camera->EmplaceComponent<GUIComponent>();
+	auto doc = g->AddDocument("ui.rml");
+	
+	struct SliderHandler : public Rml::EventListener {
+		WeakRef<Level> level;
+		decltype(doc) document;
+		SliderHandler(decltype(level) l, decltype(document) d) : level(l), document(d) {}
+
+		void ProcessEvent(Rml::Event& event) final {
+			auto field = static_cast<Rml::ElementFormControlInput*>(document->GetElementById("numobjs"));
+			auto value = std::stoi(field->GetValue());
+
+			auto world = level.lock();
+			const auto objects = world->GetAllComponentsOfType<ObjectMarker>();
+			for (const auto& obj : objects) {
+				obj->getOwner().lock()->GetComponent<StaticMesh>().value()->Enabled = false;
+			}
+			int i = 1;
+			for (const auto& obj : objects) {
+				obj->getOwner().lock()->GetComponent<StaticMesh>().value()->Enabled = true;
+				if (i >= value) {
+					break;
+				}
+				i++;
+			}
+			App::DispatchMainThread([&,value] {
+				document->GetElementById("readout")->SetInnerRML(StrFormat("Number of objects: {}", value));
+			});
+		}
+	};
+	doc->GetElementById("numobjs")->AddEventListener("change", new SliderHandler(static_pointer_cast<Level>(shared_from_this()), doc));
+
+	// inputs
+	auto im = App::inputManager = std::make_shared<RavEngine::InputManager>();
+	im->AddAxisMap("MouseX", Special::MOUSEMOVE_X);
+	im->AddAxisMap("MouseY", Special::MOUSEMOVE_Y);
+	im->BindAxis("MouseX", g, &GUIComponent::MouseX, CID::ANY, 0);
+	im->BindAxis("MouseY", g, &GUIComponent::MouseY, CID::ANY, 0);
+	im->BindAnyAction(g);
 
 	auto lights = make_shared<Entity>();
 	lights->EmplaceComponent<AmbientLight>()->Intensity = 0.1;
@@ -59,12 +117,17 @@ void Level::OnActivate() {
 	auto hmat = make_shared<PBRMaterialInstance>(Material::Manager::AccessMaterialOfType<PBRMaterial>());
 	
 	for (int i = 0; i < 50; i++) {
-		Spawn(make_shared<StaticMeshEntity>(hmesh,hmat));
+		auto e = make_shared<StaticMeshEntity>(hmesh, hmat);
+		if (i == 0) {
+			e->GetComponent<StaticMesh>().value()->Enabled = true;
+		}
+		Spawn(e);
 	}
 
 	// load Systems
 	systemManager.EmplaceSystem<SpinSystem>();
 	systemManager.EmplaceSystem<CirculateSystem>();
+	systemManager.EmplaceTimedSystem<FPSSystem>(chrono::seconds(1));
 
 	// load lights
 	for (int i = 0; i < 5; i++) {
