adb4ef1b75b7e73a463c6f5f22f81866a46e0f60
Ravbug <ravbuganimations@gmail.com>
2021-06-26 22:27:29 -0700

    Make spinsystem more correct

--- Samples/Perf-DrawAnimated/Systems.hpp
+++ Samples/Perf-DrawAnimated/Systems.hpp
@@ -11,11 +11,10 @@ struct SpinComponent : public RavEngine::Component, public RavEngine::Queryable<
 struct SpinSystem : public RavEngine::AutoCTTI {
 	bool paused = false;
 	
-    void Tick(float fpsScale,Ref<SpinComponent> c) {
+    void Tick(float fpsScale, Ref<SpinComponent> c, Ref<RavEngine::Transform> tr) {
 		//get the entity and spin it based on the component data
-        auto e = c->getOwner().lock();
-        if (!paused && e){
-            e->transform()->LocalRotateDelta((double)fpsScale * c->spinamt);
+        if (!paused){
+            tr->LocalRotateDelta((double)fpsScale * c->spinamt);
         }
 	}
 };
