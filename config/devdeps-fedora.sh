bd6c2024ca22dc86879d766cbae9a436276a5f7a
Ravbug <ravbuganimations@gmail.com>
2021-10-21 23:49:12 -0400

    Update engine

--- config/devdeps-fedora.sh
+++ config/devdeps-fedora.sh
@@ -1,2 +1,2 @@
 
-sudo dnf install cmake g++ git gdb htop ninja-build libatomic wayland-devel libxkbcommon-devel mesa-libEGL-devel alsa-lib-devel libXext-devel
+sudo dnf install cmake g++ git gdb htop ninja-build libatomic wayland-devel libxkbcommon-devel mesa-libEGL-devel alsa-lib-devel libXext-devel -y
--- config/livecd-clean-fedora.sh
+++ config/livecd-clean-fedora.sh
@@ -1,2 +1,2 @@
 
-sudo dnf remove *calendar* *calculator* *anaconda* *maps* *boxes* *system-monitor* libreoffice* *tour* *problem* *boxes* *contacts* *clocks* *scanner* *rhythmbox* *characters* *scan* *document* *logs* *software* --skip-broken
+sudo dnf remove *calendar* *calculator* *anaconda* *maps* *boxes* *system-monitor* libreoffice* *tour* *problem* *boxes* *contacts* *clocks* *scanner* *rhythmbox* *characters* *scan* *document* *logs* *software* --skip-broken -y
