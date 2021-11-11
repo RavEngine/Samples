180f3ba9a793377324c85b294a4686325fbd3556
Ravbug <ravbuganimations@gmail.com>
2021-11-11 17:06:48 -0500

    Changes to get Hello Cube compiling on the new system

--- Samples/HelloCube/App.cpp
+++ Samples/HelloCube/App.cpp
@@ -1,6 +1,9 @@
 #include "AppInfo.hpp"
 #include <RavEngine/App.hpp>
 #include <RavEngine/StaticMesh.hpp>
+#include <RavEngine/World.hpp>
+#include <RavEngine/CameraComponent.hpp>
+#include <RavEngine/Transform.hpp>
 
 using namespace RavEngine;
 using namespace std;
@@ -18,6 +21,12 @@ struct HelloCubeApp : public RavEngine::App {
 	void OnStartup(int argc, char** argv) final;
 };
 
+struct BasicEntity : public RavEngine::Entity{
+    void Create(){
+        EmplaceComponent<Transform>();
+    }
+};
+
 // After defining an App, define a World by creating a class that derives from RavEngine::World.
 // A world is akin to a Scene in Unity or a Level in Unreal. Worlds contain all the action in a game.
 struct HelloCubeWorld : public RavEngine::World {
@@ -31,13 +40,16 @@ struct HelloCubeWorld : public RavEngine::World {
 		// If this world will be loaded and unloaded, do not do initialization in this method as
 		// each time the world is made active, code here runs.
 
-		// Create an Entity. Entities are akin to GameObjects in Unity.
-		// Many RavEngine features leverage reference counting, so use std::make_shared to construct them.
-		auto cubeEntity = make_shared<Entity>();
+		// Create an Entity. RavEngine uses an ECS, so entities are not like Unity GameObjects.
+        // Instead, think of an Entity as a handle to a collection of components stored elsewhere.
+        // Unlike other ECS implementations, Entity handles are global, and also have convenience
+        // member functions.
+        // When you call this, it immediately creates the entity in the world.
+		auto cubeEntity = CreatePrototype<BasicEntity>();
 
 		// We want to display a cube in this world. RavEngine uses a component-based composition model 
 		// for describing scene objects. However, you can also subclass RavEngine::Entity and perform
-		// setup inside its constructor for Unreal-style inheritance. 
+		// setup inside Entity::Create for Unreal-style inheritance. 
 
 		// We will start by loading the cube mesh. The cube is one of the default primitives that comes with RavEngine.
 		auto cubeMesh = MeshAsset::Manager::GetDefault("cube.obj");
@@ -51,38 +63,31 @@ struct HelloCubeWorld : public RavEngine::World {
 		// Note that all StaticMeshes must have a material bound to them. A StaticMesh without a material will cause
 		// the engine to crash, as that is an invalid state.
 		// The EmplaceComponent method constructs the component inline and attaches it to the entity.
-		cubeEntity->EmplaceComponent<StaticMesh>(cubeMesh, cubeMat);
+        cubeEntity.EmplaceComponent<StaticMesh>(cubeMesh, cubeMat);
 
-		// Now that we have our cube, let's place it in the world.
-		Spawn(cubeEntity);
 
 		// We want to be able to see our cube, so we need a camera. In RavEngine, cameras are also components, so 
 		// we need another entity to hold that. 
-		auto cameraEntity = make_shared<Entity>();
+		auto cameraEntity = CreatePrototype<BasicEntity>();
 
 		// Create the CameraComponent. The EmplaceComponent call returns an owning pointer to the constructed component.
 		// Be wary of storing these to avoid reference cycles that lead to memory leaks. 
-		auto cameraComponent = cameraEntity->EmplaceComponent<CameraComponent>();
+		auto& cameraComponent = cameraEntity.EmplaceComponent<CameraComponent>();
 
 		// By default, cameras are disabled, and do not render. We must enable our camera for it to display anything in the world.
-		cameraComponent->SetActive(true);
+		cameraComponent.SetActive(true);
 
-		// Add the camera to the world.
-		Spawn(cameraEntity);
 
 		// As of this line, the camera is inside the cube. To rectify this, let's place the cube in front of the camera.
-		cubeEntity->GetTransform()->LocalTranslateDelta(vector3(0,0,-5));
+        cubeEntity.GetTransform().LocalTranslateDelta(vector3(0,0,-5));
 
 		// We want some lighting on our cube. In RavEngine, lights are also components, so we'll need an entity for those.
-		auto lightsEntity = make_shared<Entity>();
-		lightsEntity->EmplaceComponent<DirectionalLight>();					// a light that mimics the sun
-		lightsEntity->EmplaceComponent<AmbientLight>()->Intensity = 0.2;	// a weak fill light that affects all surfaces equally
+		auto lightsEntity = CreatePrototype<BasicEntity>();
+		lightsEntity.EmplaceComponent<DirectionalLight>();					// a light that mimics the sun
+		lightsEntity.EmplaceComponent<AmbientLight>().Intensity = 0.2;	// a weak fill light that affects all surfaces equally
 
 		// Lights use the transformation of their parent entity to determine their rotation and position.
-		lightsEntity->GetTransform()->LocalRotateDelta(vector3{ glm::radians(45.0),glm::radians(45.0),0 });
-
-		// add the lights to the world
-		Spawn(lightsEntity);
+		lightsEntity.GetTransform().LocalRotateDelta(vector3{ glm::radians(45.0),glm::radians(45.0),0 });
 	}
 
 	// The engine calls it synchronously every tick after the rest of the pipeline has finished processing.
@@ -91,29 +96,24 @@ struct HelloCubeWorld : public RavEngine::World {
 		// We can hold onto an entity as a class variable, but I'll instead use this opportunity to demonstrate the query system.
 		// RavEngine worlds can be queried extremely efficiently by component type. Since we know that this world contains
 		// one static mesh, we can use that to get our entity.
-		auto meshCompOpt = GetComponent<StaticMesh>();
+		auto& meshComp = GetComponent<StaticMesh>();
 
-		// however, that call gives us a component pointer optional. Components store their owners as weak pointers, so we can use
-		// GetOwner to get the owner.
-		if (meshCompOpt) {
-			auto meshComp = meshCompOpt.value();
-			auto entity = meshComp->GetOwner().lock();	//convert to strong pointer
+        auto entity = meshComp.GetOwner();	//convert to strong pointer
 
-			// Let's spin our cube.
-			// RavEngine expects rotations in radians. Use glm::radians to convert to degrees.
-			auto rotVec = vector3(glm::radians(1.0), glm::radians(2.0), glm::radians(-0.5));	
+        // Let's spin our cube.
+        // RavEngine expects rotations in radians. Use glm::radians to convert to degrees.
+        auto rotVec = vector3(glm::radians(1.0), glm::radians(2.0), glm::radians(-0.5));
 
-			// Unlike most engines, RavEngine's render engine is asynchronous. This means that the engine 'ticks'
-			// at a different rate that it renders frames. One tick =/= one frame. In addition, if the engine were to fall behind, 
-			// we need to ensure our game does not run in slow motion. To accomplish this, we simply multiply our movements by 
-			// a scale factor passed into this method. RavEngine has already calcuated the scale factor for us. This is *not* a deltaTime,
-			// like in Unity.
-			rotVec *= tickrateScale;
+        // Unlike most engines, RavEngine's render engine is asynchronous. This means that the engine 'ticks'
+        // at a different rate that it renders frames. One tick =/= one frame. In addition, if the engine were to fall behind,
+        // we need to ensure our game does not run in slow motion. To accomplish this, we simply multiply our movements by
+        // a scale factor passed into this method. RavEngine has already calcuated the scale factor for us. This is *not* a deltaTime,
+        // like in Unity.
+        rotVec *= tickrateScale;
 
-			// Entities can also be queried efficiently for components, just like worlds. Since Transforms are 
-			// a very frequent access, Entities provide the convenience function GetTransform, to use instead.
-			entity->GetComponent<Transform>().value()->LocalRotateDelta(rotVec);
-		}
+        // Entities can also be queried efficiently for components, just like worlds. Since Transforms are
+        // a very frequent access, Entities provide the convenience function GetTransform, to use instead.
+        entity.GetComponent<Transform>().LocalRotateDelta(rotVec);
 	}
 };
 
