90904216241f72dfb9fb65eb1c809c3ec1d9b25d
Ravbug <ravbuganimations@gmail.com>
2021-11-13 17:11:06 -0500

    Fix the GUI sample

--- Samples/GUI/World.cpp
+++ Samples/GUI/World.cpp
@@ -1,7 +1,7 @@
 #include "World.hpp"
 
 #include <RavEngine/CameraComponent.hpp>
-#include <RavEngine/Entity.hpp>
+#include <RavEngine/GameObject.hpp>
 #include <RavEngine/StaticMesh.hpp>
 #include <RavEngine/GUI.hpp>
 #include <RavEngine/InputManager.hpp>
@@ -14,7 +14,7 @@ using namespace std;
 struct SingleEntityMarker : public Component, public Queryable<SingleEntityMarker>{};
 
 struct FPSSystem : public AutoCTTI {
-	void Tick(float scale, Ref<SingleEntityMarker>){
+	void operator()(float scale, const SingleEntityMarker& s) const{
 		App::DispatchMainThread([](){
 			App::SetWindowTitle(StrFormat("RavEngine GUIKitchenSink | {} - {} TPS, {} FPS ({} ms)", App::GetRenderEngine().GetCurrentBackendName(), (int)App::CurrentTPS(), (int)App::GetRenderEngine().GetCurrentFPS(), (int)App::GetRenderEngine().GetLastFrameTime()).c_str());
 		});
@@ -25,47 +25,43 @@ struct FPSSystem : public AutoCTTI {
 void ::World::OnActivate(){
 	//camera and cube
 	
-	Ref<Entity> camlights = make_shared<Entity>();
-	camlights->EmplaceComponent<CameraComponent>()->SetActive(true);
-	camlights->EmplaceComponent<SingleEntityMarker>();
-	camlights->EmplaceComponent<AmbientLight>()->Intensity = 0.2;
-	camlights->GetTransform()->LocalTranslateDelta(vector3(0,0,5));
+	auto camlights = CreatePrototype<GameObject>();
+	camlights.EmplaceComponent<CameraComponent>().SetActive(true);
+	camlights.EmplaceComponent<SingleEntityMarker>();
+	camlights.EmplaceComponent<AmbientLight>().Intensity = 0.2;
+	camlights.GetTransform().LocalTranslateDelta(vector3(0,0,5));
 	
-	Ref<Entity> dirlight = make_shared<Entity>();
-	dirlight->EmplaceComponent<DirectionalLight>();
-	dirlight->GetTransform()->LocalRotateDelta(vector3(glm::radians(45.0),glm::radians(45.0),0));
+	auto dirlight = CreatePrototype<GameObject>();
+	dirlight.EmplaceComponent<DirectionalLight>();
+	dirlight.GetTransform().LocalRotateDelta(vector3(glm::radians(45.0),glm::radians(45.0),0));
 	
-	cube = make_shared<Entity>();
-	auto cubemesh = cube->EmplaceComponent<StaticMesh>(MeshAsset::Manager::GetDefault("cube.obj"),make_shared<PBRMaterialInstance>(Material::Manager::Get<PBRMaterial>()));
+	cube = CreatePrototype<GameObject>();
+	auto& cubemesh = cube.EmplaceComponent<StaticMesh>(MeshAsset::Manager::GetDefault("cube.obj"),make_shared<PBRMaterialInstance>(Material::Manager::Get<PBRMaterial>()));
 	
-	Ref<Entity> gui = make_shared<Entity>();
-	auto doc = gui->EmplaceComponent<GUIComponent>();
-	doc->AddDocument("sink.rml");
+	auto gui = CreatePrototype<Entity>();
+	auto& doc = gui.EmplaceComponent<GUIComponent>();
+	doc.AddDocument("sink.rml");
 	
-	Spawn(camlights);
-	Spawn(cube);
-	Spawn(dirlight);
-	Spawn(gui);
-	
-	doc->Debug();
+	doc.Debug();
 	
 	//input manager
 	Ref<InputManager> im = make_shared<InputManager>();
 	im->AddAxisMap("MouseX", Special::MOUSEMOVE_X);
 	im->AddAxisMap("MouseY", Special::MOUSEMOVE_Y);
 	
-	im->BindAxis("MouseX", doc, &GUIComponent::MouseX, CID::ANY,0);
-	im->BindAxis("MouseY", doc, &GUIComponent::MouseY, CID::ANY,0);
-	im->BindAnyAction(doc);
+    ComponentHandle<GUIComponent> g(gui);
+	im->BindAxis("MouseX", g, &GUIComponent::MouseX, CID::ANY,0);
+	im->BindAxis("MouseY", g, &GUIComponent::MouseY, CID::ANY,0);
+	im->BindAnyAction(g->GetData());
 	
 	App::inputManager = im;
 	
 	//FPS updating system
-	systemManager.RegisterTimedSystem(make_shared<FPSSystem>(), std::chrono::seconds(1));
+	EmplaceTimedSystem<FPSSystem,SingleEntityMarker>(std::chrono::seconds(1));
 
 	Debug::Log("{} with ID: {}",type_name<GUIComponent>(),CTTI<GUIComponent>());
 }
 
 void ::World::PostTick(float scale){
-	cube->GetTransform()->LocalRotateDelta(vector3(glm::radians(1.0) * scale,glm::radians(-1.0 * scale),glm::radians(0.5) * scale));
+    cube.GetTransform().LocalRotateDelta(vector3(glm::radians(1.0) * scale,glm::radians(-1.0 * scale),glm::radians(0.5) * scale));
 }
--- Samples/GUI/World.hpp
+++ Samples/GUI/World.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <RavEngine/World.hpp>
+#include <RavEngine/GameObject.hpp>
 
 class World : public RavEngine::World{
 	void OnActivate() override;
 	void PostTick(float) override;
 protected:
-	Ref<RavEngine::Entity> cube;
+	RavEngine::GameObject cube;
 };
