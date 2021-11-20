2e1bba498aa24719c5c719100d1b9ec432c49b4a
Ravbug <ravbuganimations@gmail.com>
2021-11-20 03:48:42 +0000

    Ubuntu scripts

---
+++ config/devdeps-ubuntu.sh
@@ -0,0 +1,2 @@
+sudo apt update
+sudo apt install git build-essential libasound2-dev libx11-dev cmake libxext-dev htop libgl-dev -y
---
+++ config/livecd-clean-ubuntu.sh
@@ -0,0 +1,2 @@
+sudo apt remove rhythmbox libreoffice* gnome-mines gnome-mahjongg gnome-sudoku remmina transmission* thunderbird* *shotwell* *gstreamer* gnome-todo gnome-calculator gnome-logs gnome-font-viewer simple-scan gnome-characters *deja* eog *language-selector* aisleriot -y
+sudo apt autoremove -y
