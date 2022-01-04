5439fd45577846bd2b0d5f640f4fdf6ae5f836aa
Ravbug <ravbuganimations@gmail.com>
2022-01-04 06:17:56 -0800

    Update engine, disable vsync

--- Samples/Perf-DrawAnimated/App.cpp
+++ Samples/Perf-DrawAnimated/App.cpp
@@ -13,7 +13,7 @@ using namespace RavEngine;
 
 void Performance_C::OnStartup(int argc, char **argv){
 	//setup video settings
-	RenderEngine::VideoSettings.vsync = true;
+	RenderEngine::VideoSettings.vsync = false;
 	RenderEngine::VideoSettings.width = 1280;
 	RenderEngine::VideoSettings.height = 720;
 	
