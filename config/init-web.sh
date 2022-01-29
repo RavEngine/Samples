f5a885cb5174619bf8e3904ff0e494b21c20fd21
Ravbug <ravbuganimations@gmail.com>
2022-01-29 11:44:13 -0500

    Add resource embedding to samples

--- config/init-web.sh
+++ config/init-web.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ..
-mkdir -p build && mkdir -p build/web && cd build/web && cmake -DCMAKE_TOOLCHAIN_FILE=../../../emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake ../..
+mkdir -p build && mkdir -p build/web && cd build/web && cmake -DCMAKE_BUILD_TYPE=Debug -Wno-dev -DCMAKE_TOOLCHAIN_FILE=../../../emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake ../..
