2f5f3bc36fed97a4173b4b2d5baf46e3a3c53c32
Ravbug <ravbuganimations@gmail.com>
2022-03-03 18:37:41 -0800

    Simpler case

--- Samples/Shadows/main.cpp
+++ Samples/Shadows/main.cpp
@@ -3,12 +3,19 @@
 #include <RavEngine/GameObject.hpp>
 #include <RavEngine/CameraComponent.hpp>
 #include <RavEngine/StaticMesh.hpp>
+#include <RavEngine/InputManager.hpp>
 #include "AppInfo.hpp"
 
 using namespace RavEngine;
+using namespace std;
 
 struct Level : public RavEngine::World{
 	GameObject tri;
+
+	void Flip() {
+		tri.GetTransform().LocalRotateDelta(vector3(0, glm::radians(90.f), 0));
+	}
+
 	Level() {
 		// load ground plane
 		auto ground = CreatePrototype<GameObject>();
@@ -18,23 +25,32 @@ struct Level : public RavEngine::World{
 		// load casting triangle
 		tri = CreatePrototype<GameObject>();
 		auto trimat = New<PBRMaterialInstance>(Material::Manager::Get<PBRMaterial>());
+		trimat->doubleSided = true;
 		trimat->SetAlbedoColor({ 1,0,0,1 });
 		tri.EmplaceComponent<StaticMesh>(MeshAsset::Manager::GetDefault("triangle.obj"),trimat);
-		tri.GetTransform().LocalTranslateDelta(vector3(0,1,0));
+		tri.GetTransform().LocalTranslateDelta(vector3(-1.5, 1, 0));
+		tri.GetTransform().LocalRotateDelta(vector3(glm::radians(90.f), glm::radians(90.f), glm::radians(180.f)));
+		tri.GetTransform().LocalRotateDelta(vector3(0, glm::radians(90.f), 0));
 
 		// create lights and camera
 		auto camera = CreatePrototype<GameObject>();
 		camera.EmplaceComponent<CameraComponent>().SetActive(true);
-		camera.GetTransform().LocalTranslateDelta(vector3(0, 2, 5));
-		camera.GetTransform().LocalRotateDelta(vector3(glm::radians(-20.f), 0, 0));
+		camera.GetTransform().LocalTranslateDelta(vector3(0, 1, 5));
+		//camera.GetTransform().LocalRotateDelta(vector3(glm::radians(-20.f), 0, 0));
 
 		auto light = CreatePrototype<GameObject>();
 		light.EmplaceComponent<DirectionalLight>().debugEnabled = true;
 		light.EmplaceComponent<AmbientLight>().Intensity = 0.2;
-		light.GetTransform().LocalRotateDelta(vector3(glm::radians(45.0), glm::radians(45.0), 0));
+		light.GetTransform().LocalRotateDelta(vector3(0, 0, glm::radians(90.f)));
+
+		auto im = GetApp()->inputManager = RavEngine::New<InputManager>();
+		im->AddActionMap("flip", SDL_SCANCODE_F);
+		im->BindAction("flip", GetInput(this), &Level::Flip, Pressed, CID::ANY);
+
 	}
 	void PostTick(float fpsScale) final {
-		tri.GetTransform().LocalRotateDelta(vector3(0,glm::radians(0.3 * fpsScale),0));
+		//tri.GetTransform().LocalRotateDelta(vector3(0,glm::radians(0.3 * fpsScale),0));
+		//tri.GetTransform().LocalRotateDelta(vector3(glm::radians(0.3 * fpsScale), 0, 0));
 	}
 };
 
