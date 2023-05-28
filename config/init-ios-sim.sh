5b3a29dd300a5dcaa2ec4e0d254738859a83471c
Ravbug <ravbug@norely.users.github.io>
2023-05-27 20:32:27 -0700

    Simplify iOS/tvOS configures

--- config/init-ios-sim.sh
+++
@@ -1,2 +0,0 @@
-cd ..
-mkdir -p build && mkdir -p build/ios-sim && cd build/ios-sim && cmake -Wno-dev -G "Xcode" -DENABLE_VISIBILITY=ON -DCMAKE_TOOLCHAIN_FILE=../../RavEngine/deps/ios/ios.toolchain.cmake -DENABLE_ARC=OFF -DDEPLOYMENT_TARGET=14.0 -DPLATFORM=SIMULATOR64 ../..
